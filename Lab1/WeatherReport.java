b0c8e2f2793000ceab421e95c2bc420d24c426f6
Daniel Homan <danielhoman31@gmail.com>
2019-02-25 20:15:17 +0000

    Changed || to &&

--- Lab1/WeatherReport.java
+++ Lab1/WeatherReport.java
@@ -9,16 +9,16 @@ public class WeatherReport{
         Scanner sc = new Scanner(System.in);
         int in = sc.nextInt();
         
-        if(in>=0 || in<=8){
+        if(in>=0 && in<=8){
             System.out.println("It is cold outside, bring a jacket!");
         }
-        else if(in >= 9 || in <= 16){
+        else if(in >= 9 && in <= 16){
             System.out.println("The sun is coming out, and it is getting warmer");
         }
-        else if(in>=17 || in<=32){
+        else if(in>=17 && in<=32){
             System.out.println("Time to sit beside the pool and relax");
         }
-        else if(in>=33 || in<=45){
+        else if(in>=33 && in<=45){
             System.out.println("Too hot, can't move!");
         }
         else{
