66307d421936ad7309ccb32624fd6f033a4069d9
Daniel Homan <danielhoman31@gmail.com>
2019-03-15 12:10:27 +0000

    Solution is done recursively, no regex.

--- Lab5/FindSubStrings.java
+++ Lab5/FindSubStrings.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
 public class FindSubStrings{
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-    String check = sc.nextLine();
-    int amount = sc.nextInt();
-    sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String sub = sc.nextLine();
+        int x = sc.nextInt();
+        sc.close();
+        System.out.println(checkForSub(s,sub,x));
+    }
 
-    System.out.println(checkForSub(s, check, amount));
-  }
+    public static boolean checkForSub(String s, String sub, int times){
 
+        if(times==0){
+            return true;
+        }
 
-  public static boolean checkForSub(String x, String y, int times){
+        if(s.length()<sub.length()){
+            return false;
+        }
 
-    String regex = "(.*" + y + ".*){" + times + "}";
-    return (x.matches(regex));
-
-  }
-
+        if(s.substring(0, sub.length()).equals(sub)){
+            return checkForSub(s.substring(sub.length()), sub, times-1);
+        } else{
+            return checkForSub(s.substring(sub.length()), sub, times);
+        }
+    }
 }
